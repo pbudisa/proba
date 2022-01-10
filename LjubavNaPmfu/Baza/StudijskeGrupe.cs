1f28919d8bb1aa196f7d6e1beb167a4fab69c870
Petra <pbudisa@pmfst.hr>
2022-01-11 00:44:16 +0100

    popravljeno?

---
+++ LjubavNaPmfu/Baza/StudijskeGrupe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LjubavNaPmfu.Baza
+{
+    public partial class StudijskeGrupe
+    {
+        public StudijskeGrupe()
+        {
+            Korisnik = new HashSet<Korisnik>();
+        }
+
+        public int IdG { get; set; }
+        public string Naziv { get; set; }
+
+        public virtual ICollection<Korisnik> Korisnik { get; set; }
+    }
+}
